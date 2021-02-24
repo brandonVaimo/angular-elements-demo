a6f25984264afbfc99616620d4146009563df855
Brandon Bezuidenhout <brandon.bezuidenhout@vaimo.com>
2021-02-24 08:41:35 +0200

    Init Projects Directory

---
+++ projects/components/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of components
+ */
+
+export * from './lib/Default/components.service';
+export * from './lib/Default/components.component';
+export * from './lib/Default/components.module';
